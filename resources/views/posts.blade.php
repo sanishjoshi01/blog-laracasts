b1fc542a337b7c059315598349597ce10b1dd1d3
sanishjoshi01 <sandeshjoshi2211@gmail.com>
2024-03-31 14:48:48 +0545

    make a route and link to it

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+
+        <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum, nisi totam adipisci, reprehenderit dicta et assumenda perferendis ea expedita possimus fugiat sed veniam dolorum placeat maiores praesentium blanditiis in molestias architecto excepturi veritatis. Omnis officia ab nesciunt! Non repellendus quibusdam soluta natus quo. Libero in deserunt aperiam a iste? Quod.
+        </p>
+    </article>
+    
+    <article>
+        <h1><a href="/post">My Second Post</a></h1>
+
+        <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum, nisi totam adipisci, reprehenderit dicta et assumenda perferendis ea expedita possimus fugiat sed veniam dolorum placeat maiores praesentium blanditiis in molestias architecto excepturi veritatis. Omnis officia ab nesciunt! Non repellendus quibusdam soluta natus quo. Libero in deserunt aperiam a iste? Quod.
+        </p>
+    </article>
+
+    <article>
+        <h1><a href="/post">My Third Post</a></h1>
+
+        <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum, nisi totam adipisci, reprehenderit dicta et assumenda perferendis ea expedita possimus fugiat sed veniam dolorum placeat maiores praesentium blanditiis in molestias architecto excepturi veritatis. Omnis officia ab nesciunt! Non repellendus quibusdam soluta natus quo. Libero in deserunt aperiam a iste? Quod.
+        </p>
+    </article>
+</body>
