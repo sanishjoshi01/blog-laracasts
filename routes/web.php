5f0ca1ccc5d837a87b03a68e6b703144da4a8df2
sanishjoshi01 <sandeshjoshi2211@gmail.com>
2024-04-01 13:18:10 +0545

    yaml front matter, document

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,28 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Models\Post;
+use Illuminate\Support\Facades\File;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 Route::get('/', function () {
+    $files = File::files(resource_path("posts/"));
+
+    $posts = [];
+
+    foreach ($files as $file) {
+        $document = YamlFrontMatter::parseFile($file);
+
+        $posts[] = new Post(
+            $document->title,
+            $document->excerpt,
+            $document->date,
+            $document->body(),
+            $document->slug,
+        );
+    }
+
     return view('posts', [
-        'posts' => Post::all()
+        'posts' => $posts
     ]);
 });
 
