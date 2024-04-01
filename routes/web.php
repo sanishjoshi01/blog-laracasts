00c21a80d35aadefa814112cd786b96c7a9011a2
sanishjoshi01 <sandeshjoshi2211@gmail.com>
2024-04-01 12:29:01 +0545

    caching

--- routes/web.php
+++ routes/web.php
@@ -8,16 +8,13 @@ Route::get('/', function () {
 
 
 Route::get('posts/{post}', function ($slug) {
-
-    $path = __DIR__ . "/../resources/posts/{$slug}.html";
-
-    if (!file_exists($path)) {
+    if (!file_exists($path = __DIR__ . "/../resources/posts/{$slug}.html")) {
         return redirect('/');
     }
 
-    $post = file_get_contents($path);
+    $post = cache()->remember("posts.{$slug}", 1200, function () use ($path) {
+        return file_get_contents($path);
+    });
 
-    return view('post', [
-        'post' => $post
-    ]);
+    return view('post', ['post' => $post]);
 })->where('post', '[A-z_\-]+');
