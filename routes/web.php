694c2cb758772aeca0d438b4647589c6dbcfb5bc
sanishjoshi01 <sandeshjoshi2211@gmail.com>
2024-04-05 12:10:26 +0545

    make a post model and migration

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,8 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('posts/{post}', function ($slug) {
+Route::get('posts/{post}', function ($id) {
     return view('post', [
-        'post' => Post::findOrFail($slug)
+        'post' => Post::findOrFail($id)
     ]);
 });
