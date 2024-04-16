bc8c1f7112a850fcd38f73b1c5461dc5a2a8d7c2
sanishjoshi01 <sandeshjoshi2211@gmail.com>
2024-04-16 12:26:14 +0545

    convert to blade

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,4 +1,22 @@
 <x-layout>
+   @include('_posts-header')
+
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        <x-post-featured-card />
+
+        <div class="lg:grid lg:grid-cols-2">
+            <x-post-card/>            
+            <x-post-card/>            
+        </div>
+
+        <div class="lg:grid lg:grid-cols-3">
+            <x-post-card/>            
+            <x-post-card/>            
+            <x-post-card/>            
+        </div>
+    </main>
+</x-layout>
+    {{-- 
     @foreach($posts as $post)
     <article>
         <h1>
@@ -20,4 +38,4 @@
         </div>
     </article>
     @endforeach
-</x-layout>
+ --}}
