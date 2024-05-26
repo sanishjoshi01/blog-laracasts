df80b1d7b046738b49dbfa1ddcc76e4cccc11b04
sanishjoshi01 <sandeshjoshi2211@gmail.com>
2024-05-26 14:11:44 +0545

    search function and controller

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+use App\Models\Category;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::latest();
+
+        if (request('search')) {
+            $posts
+                ->where('title', 'like', '%' . request('search') . '%')
+                ->orWhere('body', 'like', '%' . request('search') . '%');
+        }
+
+        return view('posts', [
+            'posts' => $posts->get(),
+            'categories' => Category::all()
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Models\User;
 use App\Models\Post;
 use App\Models\Category;
+use App\Http\Controllers\PostController;
 
-Route::get('/', function () {
-    return view('posts', [
-        'posts' => Post::latest()->get(),
-        'categories' => Category::all()
-    ]);
-})->name('home');
+Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('posts/{post:slug}', function (Post $post) {
     return view('post', [
